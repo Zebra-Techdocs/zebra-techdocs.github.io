b70ee3b2343aae1aba6ed144bc09fa26c17fccff
Edward Correia <edward.correia@zebra.com>
2022-09-22 08:15:27 -0400

    [unplanned] HTTP -> HTTPS global-replace (fingers crossed)

--- dcs/rfid/fixedreaders/read2cloud/1-0/new_docs/html/_static/js/custom.js
+++ dcs/rfid/fixedreaders/read2cloud/1-0/new_docs/html/_static/js/custom.js
@@ -1,3 +1,3 @@
 $(document).ready(function () {
-    $('a[href^="http://"], a[href^="https://"]').not('a[class*=internal]').attr('target', '_blank');
+    $('a[href^="https://"], a[href^="https://"]').not('a[class*=internal]').attr('target', '_blank');
  });
