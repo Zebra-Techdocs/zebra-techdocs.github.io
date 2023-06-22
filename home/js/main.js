f8bb00a9f7ee809471e063c60cde1ed148f48816
nrcg87 <40235943+nrcg87@users.noreply.github.com>
2023-06-22 08:27:00 -0400

    [DLM-24709] Publish DTRK 5.3 - Add link to admin page & replace latest folder

--- home/js/main.js
+++ home/js/main.js
@@ -820,6 +820,7 @@ function populateDropdown (ddl_ver, ddl_doc) {
             }
             break;
         case '/devicetracker/5-2':
+        case '/devicetracker/5-3':
             ddl_doc.options.length = 0;
             for (i=0; i < dt_5_2_text.length; i++) {
                 createOption(ddl_doc, dt_5_2_text[i], dt_5_2_val[i]);
