9a5ec1b388adb01e639ab262a78c6a2824839538
Edward Correia <edward.correia@zebra.com>
2023-08-10 10:31:12 -0400

    [unplanned] /dcs/scanners updated, per eng

--- dcs/scanners/sdk-linux/api/reference/search/all_b.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['updatefirmwareeventid',['UpdateFirmwareEventID',['../d1/d0f/CsUserDefs_8h.html#a661b766333359b7544dab53d8e871ea0',1,'CsUserDefs.h']]]
-];
--- dcs/scanners/sdk-linux/api/reference/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['open',['Open',['../d6/d06/Cslibcorescanner__xml_8h.html#a1a16ea210b8409b100b1e1e03730b4bc',1,'Cslibcorescanner_xml.h']]]
-];
