2cf9e0e60bfe442f211184c22f301761f473f15e
nrcg87 <40235943+nrcg87@users.noreply.github.com>
2025-09-30 11:34:20 -0400

    [TUT-116369] IG 2.2 - Publish live, per Sabir (tho incomplete)

--- home/js/main.js
+++ home/js/main.js
@@ -1083,6 +1083,7 @@ function populateDropdown (ddl_ver, ddl_doc) {
         case '/identityguardian/1-7':
         case '/identityguardian/2-0':
         case '/identityguardian/2-1':
+        case '/identityguardian/2-2':
             ddl_doc.options.length = 0;
             for (i=0; i < ig_1_3_text.length; i++) {
                 createOption(ddl_doc, ig_1_3_text[i], ig_1_3_val[i]);
