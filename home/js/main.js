8bd6192b31b4ac64cb7c154b2b2d0f9dade77117
nrcg87 <40235943+nrcg87@users.noreply.github.com>
2025-10-29 11:35:14 -0400

    [TUT-117522] Publish DevCentral 4.3 / Fix tile spacing in Admin page

--- home/js/main.js
+++ home/js/main.js
@@ -824,6 +824,7 @@ function populateDropdown (ddl_ver, ddl_doc) {
         case '/devicecentral/4-0':
         case '/devicecentral/4-1':
         case '/devicecentral/4-2':
+        case '/devicecentral/4-3':
             ddl_doc.options.length = 0;
             for (i=0; i < dc_3_0_text.length; i++) {
                 createOption(ddl_doc, dc_3_0_text[i], dc_3_0_val[i]);
