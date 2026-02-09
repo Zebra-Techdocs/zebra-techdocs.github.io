1e51c7fc430cd254d14396580da9048c800f40e6
nrcg87 <40235943+nrcg87@users.noreply.github.com>
2026-02-09 11:11:35 -0500

    [TUT-122456] Publish IG 2.6

--- home/js/main.js
+++ home/js/main.js
@@ -1088,6 +1088,7 @@ function populateDropdown (ddl_ver, ddl_doc) {
         case '/identityguardian/2-2':
         case '/identityguardian/2-3':
         case '/identityguardian/2-4':
+        case '/identityguardian/2-6':
             ddl_doc.options.length = 0;
             for (i=0; i < ig_1_3_text.length; i++) {
                 createOption(ddl_doc, ig_1_3_text[i], ig_1_3_val[i]);
